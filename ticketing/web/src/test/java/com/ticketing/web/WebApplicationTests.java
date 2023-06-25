693cef9733c717b2203c5b53c3f45e1c2f59c553
Joey.ha <hsz3855@gmail.com>
2023-06-26 00:33:37 +0900

    create sub-module

--- ticketing/web/src/test/java/com/ticketing/web/WebApplicationTests.java
+++ ticketing/web/src/test/java/com/ticketing/web/WebApplicationTests.java
@@ -1,13 +1,13 @@
-package com.soojong.ticketing;
+package com.ticketing.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TicketingApplicationTests {
+class WebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
